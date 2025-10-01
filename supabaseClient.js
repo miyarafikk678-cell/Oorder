2fe08e3846a046d4d14413fcf95faf7d819e376b
miyarafikk678-cell <miyarafikk678@gmail.com>
2025-10-01 13:51:33 +0300

    Update supabaseClient.js

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,4 +1,4 @@
 import { createClient } from '@supabase/supabase-js'
-export const SUPABASE_URL = 'https://YOUR_SUPABASE_PROJECT.supabase.co'
-export const SUPABASE_ANON_KEY = 'YOUR_SUPABASE_ANON_KEY'
+export const SUPABASE_URL = 'https://lrpdetggbtqcdfaquype.supabase.co'
+export const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxycGRldGdnYnRxY2RmYXF1eXBlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTkyNjM1MTYsImV4cCI6MjA3NDgzOTUxNn0.Cnlteg7bgj2q9ONGEMd4vX2Wkv0SNQ5gHslW2uIayLg'
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
